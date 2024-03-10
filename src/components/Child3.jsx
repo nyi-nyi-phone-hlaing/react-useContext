03d2c2855285c51d7140438e2d12777c8b30b0db
Nyi Nyi Phone Hlaing <nnph123725@gmail.com>
2024-03-10 14:57:35 +0630

    more about useContext

--- src/components/Child3.jsx
+++ src/components/Child3.jsx
@@ -2,11 +2,20 @@ import { useContext } from "react";
 import { FirstContext } from "../context/FirstContext";
 
 const Child3 = () => {
-  let { userName } = useContext(FirstContext);
+  let { userName, num, setNum } = useContext(FirstContext);
   return (
     <section>
       <h1>Child 3</h1>
       <p>Company name is {userName}</p>
+      <div>
+        <p>{num}</p>
+        <button
+          onClick={() => {
+            setNum((prev) => prev + 1);
+          }}>
+          +1
+        </button>
+      </div>
     </section>
   );
 };
--- src/context/FirstContext.js
+++ src/context/FirstContext.js
@@ -1,12 +1,13 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const FirstContext = createContext(null);
 
 const FristContextProvider = (props) => {
+  let [num, setNum] = useState(0);
   let userName = "Codelab MM";
   let liveIn = "Mandalay";
   return (
-    <FirstContext.Provider value={{ userName, liveIn }}>
+    <FirstContext.Provider value={{ userName, liveIn, num, setNum }}>
       {props.children}
     </FirstContext.Provider>
   );
